5cdebf1ad13d89735ab18a00843e7e3bf3d4d623
leach.chen <leach>
2018-04-02 18:33:57 +0800

    add

--- samples/TestMaterialDesign/src/main/java/com/leachchen/testmaterialdesign/MainActivity.java
+++ samples/TestMaterialDesign/src/main/java/com/leachchen/testmaterialdesign/MainActivity.java
@@ -1,7 +1,10 @@
 package com.leachchen.testmaterialdesign;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rv_material_design = (RecyclerView)this.findViewById(R.id.rv_material_design);
+
+       new SimpleRecycleViewHelp(this,rv_material_design,
+                new ArrayList<Class>(){{
+                    add(Type1Activity.class);
+                    add(Type2Activity.class);
+                }},
+                new ArrayList<String>(){{
+                    add("样式1");
+                    add("样式2");
+                }});
     }
 }
