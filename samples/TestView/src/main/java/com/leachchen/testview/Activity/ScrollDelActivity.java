50f45f98d34f9c8b21b38213c30175096a658229
leach.chen <leach>
2018-04-12 19:06:22 +0800

    add

--- samples/TestView/src/main/java/com/leachchen/testview/Activity/ScrollDelActivity.java
+++ samples/TestView/src/main/java/com/leachchen/testview/Activity/ScrollDelActivity.java
@@ -42,7 +42,7 @@ public class ScrollDelActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll_del);
 
-        rv_recycle = (ItemRemoveRecyclerView)this.findViewById(R.id.rv_recycle);
+        rv_recycle = (RecyclerView)this.findViewById(R.id.rv_recycle);
 
         mStringList.add("aa");
         mStringList.add("aa");
