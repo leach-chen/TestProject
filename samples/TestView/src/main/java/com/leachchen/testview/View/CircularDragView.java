c5eefe68bc1712dcdec29aa9d006367ad3017873
leach.chen <leach>
2019-03-12 18:31:46 +0800

    modify

---
+++ samples/TestView/src/main/java/com/leachchen/testview/View/CircularDragView.java
@@ -0,0 +1,27 @@
+package com.leachchen.testview.View;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * ClassName:   CircularDrag.java
+ * Description:
+ * Author :     leach.chen
+ * Date:        2019/3/12 18:29
+ **/
+public class CircularDragView extends View {
+    public CircularDragView(Context context) {
+        super(context);
+    }
+
+    public CircularDragView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircularDragView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
