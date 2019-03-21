b3e57c013d7097dd8120b450f1f6f15e1ca962f8
leach.chen <leach>
2019-03-21 18:29:04 +0800

    modify

--- samples/TestIjkplayer/src/main/java/com/leachchen/testijkplayer/MainActivity.java
+++ samples/TestIjkplayer/src/main/java/com/leachchen/testijkplayer/MainActivity.java
@@ -1,13 +1,24 @@
 package com.leachchen.testijkplayer;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.leachchen.testijkplayer.media.IjkVideoView;
+
 public class MainActivity extends AppCompatActivity {
 
+    private IjkVideoView mVideoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVideoView = (IjkVideoView)findViewById(R.id.ijkplayer);
+
+        //ijkplayer.setVideoPath("http://192.168.1.254:8192");
+
+        mVideoView.setVideoURI(Uri.parse("http://192.168.1.254:8192"));
+        mVideoView.start();
+
     }
 }
