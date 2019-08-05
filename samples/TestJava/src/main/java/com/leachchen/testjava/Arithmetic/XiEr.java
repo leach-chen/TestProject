2733ef771e3bd95cf7b188e4efcb8cb61f0cdb90
leach.chen <leach>
2019-08-05 16:43:38 +0800

    modify

---
+++ samples/TestJava/src/main/java/com/leachchen/testjava/Arithmetic/XiEr.java
@@ -0,0 +1,21 @@
+package com.leachchen.testjava.Arithmetic;
+
+/**
+ * ClassName:   XiEr.java
+ * Description:
+ * Author :     leach.chen
+ * Date:        2019/8/5 14:05
+ **/
+public class XiEr {
+    public static void main(String[]args)
+    {
+        int []arrayData = {2,3,1,33,8,7,0,6,19};
+        sortData(arrayData);
+    }
+
+    private static void sortData(int []data)
+    {
+
+    }
+
+}
