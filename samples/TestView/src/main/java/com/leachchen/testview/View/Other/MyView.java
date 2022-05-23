51e08605b45d78f2bf5103b9b229ddf1083ca3bd
leach-chen <349763031@qq.com>
2022-05-23 17:42:27 +0800

    添加test view

---
+++ samples/TestView/src/main/java/com/leachchen/testview/View/Other/MyView.java
@@ -0,0 +1,64 @@
+package com.leachchen.testview.View.Other;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MyView extends View {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev)
+    {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("mytest", "MyView dispatchTouchEvent Custom: ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("mytest", "MyView dispatchTouchEvent Custom: ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("mytest", "MyView dispatchTouchEvent Custom: ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d("mytest", "MyView dispatchTouchEvent Custom: ACTION_CANCEL");
+                break;
+        }
+        return super.dispatchTouchEvent(ev);//默认也是FALSE
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev)
+    {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("mytest", "MyView onTouchEvent Custom: ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("mytest", "MyView onTouchEvent Custom: ACTION_MOVE");
+                getParent().requestDisallowInterceptTouchEvent(true);
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("mytest", "MyView onTouchEvent Custom: ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d("mytest", "MyView onTouchEvent Custom: ACTION_CANCEL");
+                break;
+        }
+        return true;//默认也是FALSE
+    }
+}
---
+++ samples/TestView/src/main/java/com/leachchen/testview/View/Other/MyViewGroup.java
@@ -0,0 +1,80 @@
+package com.leachchen.testview.View.Other;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.FrameLayout;
+
+public class MyViewGroup extends FrameLayout {
+    public MyViewGroup(Context context) {
+        super(context);
+    }
+
+    public MyViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev)
+    {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("mytest", "MyViewGroup dispatchTouchEvent Custom: ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("mytest", "MyViewGroup dispatchTouchEvent Custom: ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("mytest", "MyViewGroup dispatchTouchEvent Custom: ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d("mytest", "MyViewGroup dispatchTouchEvent Custom: ACTION_CANCEL");
+                break;
+        }
+        return super.dispatchTouchEvent(ev);//默认也是FALSE
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev)
+    {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("mytest", "MyViewGroup onInterceptTouchEvent Custom: ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("mytest", "MyViewGroup onInterceptTouchEvent Custom: ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("mytest", "MyViewGroup onInterceptTouchEvent Custom: ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d("mytest", "MyViewGroup onInterceptTouchEvent Custom: ACTION_CANCEL");
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);//默认也是FALSE
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent ev)
+    {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d("mytest", "MyViewGroup onTouchEvent Custom: ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.d("mytest", "MyViewGroup onTouchEvent Custom: ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d("mytest", "MyViewGroup onTouchEvent Custom: ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.d("mytest", "MyViewGroup onTouchEvent Custom: ACTION_CANCEL");
+                break;
+        }
+        return super.onTouchEvent(ev);//默认也是FALSE
+    }
+}
